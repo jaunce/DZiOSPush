09a22dcd1854f32f9012eaa56b2a808bd7dfbffb
jaunce <jszhang@aniu.tv>
2016-08-19 21:10:17 +0800

    tweak

--- DZiOSPush/DZiOSPush.m
+++ DZiOSPush/DZiOSPush.m
@@ -11,7 +11,7 @@
 #import "NSDictionaryUtils.h"
 #import "DZAppHandler.h"
 
-@interface DZiOSPush ()<UIAlertViewDelegate,UIApplicationDelegate>
+@interface DZiOSPush () <UIApplicationDelegate>
 
 {
     NSInteger _cbId;//方法的回调id
@@ -40,6 +40,7 @@
 - (void)initialize:(NSDictionary *)paramDict {
     _cbId = [paramDict integerValueForKey:@"cbId" defaultValue:-1];
     
+    //点击App打开
     NSDictionary *launchOptions = [DZAppHandler sharedHander].launchOptions;
     NSDictionary *userInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
     [self handleRemoteNotification:userInfo];
@@ -61,13 +62,13 @@
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
-    //后台点击推送唤醒 <iOS7
+    //后台点击推送唤醒 before iOS7
     [self handleRemoteNotification:userInfo];
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler {
     if ([UIApplication sharedApplication].applicationState == UIApplicationStateInactive) {
-        //后台点击推送唤醒 >iOS7
+        //后台点击推送唤醒 after iOS7
         [self handleRemoteNotification:userInfo];
     }
     completionHandler(UIBackgroundFetchResultNewData);
